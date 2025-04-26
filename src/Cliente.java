13b751c669ac0656b21e816023506aca69f18678
Azevedo <weldysona@gmail.com>
2025-04-25 21:07:18 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,52 @@
+
+
+public class Cliente {
+
+        private String nome;
+        private String cpf;
+        private String profissao;
+        private double salario;
+
+
+        public  Cliente(String nome, String cpf){
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+    public Cliente() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,6 @@
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(int agencia, int numero, Cliente titular){
+        super(agencia, numero, titular);
+    }
+
+}
